4947636207ffa04a16857907f10682f89af52923
vvasuki <vishvas.vasuki@gmail.com>
2018-09-22 21:27:09 -0700

    Content update.

---
+++ rebuild_github_sites.py
@@ -0,0 +1,19 @@
+import subprocess
+import logging
+
+github_io_repos = [
+    "~/sanskrit/matturu",
+    "~/sanskrit/mbh-audio",
+    "~/sanskrit/people",
+    "~/sanskrit-coders/site",
+    "~/vvasuki-git/notes",
+    "~/vvasuki-git/site",
+    "~/xetram/rekhaa-catering",
+    "~/sanskrit/people",
+    "~/hindutva/site",
+]
+
+for repo in github_io_repos:
+    logging.info(subprocess.check_output(
+        "cd %s; %s" % (repo, 'git commit --allow-empty -m "Force rebuild of site"'),
+        stderr=subprocess.STDOUT, shell=True))
