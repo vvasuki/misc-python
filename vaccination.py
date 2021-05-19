3ce5b42047058855bf584420dd708c667f6e0aad
vvasuki <vishvas.vasuki@gmail.com>
2021-05-19 07:22:34 +0530

    CU

--- vaccination.py
+++ vaccination.py
@@ -14,6 +14,7 @@ HDRS = {
 }
 
 def make_request(url, headers):
+  # print(url)
   ctx = ssl.create_default_context()
   ctx.check_hostname = False
   ctx.verify_mode = ssl.CERT_NONE
@@ -32,7 +33,7 @@ def get_district_ids_for_state(state_id):
   url = DISTRICTS_URL_PREFIX.format(state_id)
   return [district["district_id"] for district in make_request(url, HDRS)["districts"]]
 
-def get_appt_locs_for_state_id(district_ids=[265, 276, 294], age=45, min_slots=0):
+def get_appt_locs_for_district_ids(district_ids=[265, 276, 294], age=45, min_slots=0):
   APPT_URL_PATTERN = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}"
   today = datetime.now().strftime("%d-%m-%Y")
 
@@ -42,13 +43,23 @@ def get_appt_locs_for_state_id(district_ids=[265, 276, 294], age=45, min_slots=0
     for center in make_request(APPT_URL_PATTERN.format(district_id, today), HDRS)["centers"]:
       for session in center["sessions"]:
         if session["min_age_limit"] <= age and session["available_capacity"] >= min_slots:
-          print("Vaccine: {} available at {} in {}, {} with pincode {} on {} and {} slots are free. It is a {} slot".format(session["vaccine"], center["name"], center["district_name"], center["state_name"], center["pincode"], session["date"], session["available_capacity"], center["fee_type"]))
+          print("""\
+          =======
+          {} available at {} in {} {} with pincode {} on {} 
+          {} slots - doese-1 {}, dose-2 {}
+          Fee type: {}
+          """.format(session["vaccine"], center["name"], center["district_name"], center["state_name"], 
+                     center["pincode"], session["date"], session["available_capacity"], session["available_capacity_dose1"], session["available_capacity_dose2"], 
+                     center["fee_type"]))
 
-  # while True:
-  #     get_appt_locs_for_state_id(state_id, 18)
-  #     sys.stdout.flush()
-  #     time.sleep(60)
+
+def continuous_query():
+  while True:
+    get_appt_locs_for_district_ids(age=18, min_slots=0)
+    sys.stdout.flush()
+    time.sleep(60)
+
 
 if __name__ == '__main__':
-  min_slots = int(sys.argv[2]) if len(sys.argv) > 2 else 0
-  get_appt_locs_for_state_id(age=18, min_slots=min_slots)
+  # get_appt_locs_for_district_ids(age=18, min_slots=1)
+  continuous_query()
