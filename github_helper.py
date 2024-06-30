12a2a1240b157567a30c8b8df4b36c8d87434bc9
vvasuki <vishvas.vasuki@gmail.com>
2024-06-30 15:40:45 +0530

    sd

--- github_helper.py
+++ github_helper.py
@@ -15,18 +15,18 @@ website_repos = {
   "hindutva": ["hindutva.github.io", "mantra-manuals", ],
   "become-hindu": ["become-hindu.github.io",  ],
   "sanskrit": ["sanskrit.github.io", ],
-  "sanskrit_coders": ["sanskrit-coders.github.io"] ,
-  "vishvAsa": ["note", "vishvAsa.github.io", "AgamaH", "jyotiSham", "mImAMsA", "rahaShTippanyaH", "vedAH_Rk", "bhAShAntaram", "kalpAntaram", "notes", "sanskrit", "vedAH_sAma", "devaH", "kannaDa", "pALi", "tipiTaka", "vedAH_yajuH", "vishvAsa.github.io"],
+  "sanskrit-coders": ["sanskrit-coders.github.io"] ,
+  "vishvAsa": ["notes", "vishvAsa.github.io", "AgamaH", "jyotiSham", "mImAMsA", "rahaShTippanyaH", "vedAH_Rk", "bhAShAntaram", "kalpAntaram", "notes", "sanskrit", "vedAH_sAma", "devaH", "kannaDa", "pALi", "tipiTaka", "vedAH_yajuH", "vishvAsa.github.io"],
   "vvasuki-git": ["vvasuki.github.io"]
 }
 
 reg_repos = {
-  "sanskrit-coders": ["sanskrit-data", "autokey-scripts_sa", "db-interface", "chandas", "audio_utils", 
+  "sanskrit-coders": ["sanskrit_data", "autokey-scripts_sa", "db-interface", "chandas", "audio_utils", 
                       "sanskrit_data", "video_curation", "audio_curation", 
                       "curation_utils", "autokey-scripts_sa", 
                       "rss-feeds", "doc_curation", ],
-  "sanskrit": ["m17n-db-indic",  "sanskrit-fonts", "indic_transliteration_scala",  "sanscript.js",],
-  "indic-dict": ["stardict-sanskrit", "stardict-tibetan", "stardict-test", "stardict-index", "stardict-english", "stardict-sanskrit-vyAkaraNa", "stardict-sanskrit-kAvya", "stardict-hindi", "stardict-dictionary-updater", "stardict-sanskrit-student", "stardict-telugu", "stardict-malayalam", "stardict-pali", "Stardict dictionaries for Pali and Buddhism.", "stardict-ayurveda", "stardict-marathi", "stardict-tamil", "stardict-gujarati", "stardict-oriya", "stardict-bengali", "stardict-assamese", "stardict-panjabi", "stardict-sinhala", "stardict-prakrit", "stardict-kashmiri", "stardict-kannada", "stardict-divehi", "stardict-nepali", "stardict-urdu", "stardict-indic-update-aur", "StarDict-1", "SanskritDictionariesInstaller", "stardict", "jstardict", "sanDict", "pystardict", "dict-tools", "dict-curation", "indic-dict.github.io", "dsal-scraper", "osx-sanskrit", ],
+  "sanskrit": ["ashtadhyayi_com_transforms", "raw_etexts_private", "telugu-texts",  "raw_etexts", "sanskrit-documents-dump",  "raw_etexts_english" ],
+  "indic-dict": ["stardict-sanskrit", "stardict-tibetan", "stardict-test", "stardict-index", "stardict-english", "stardict-sanskrit-vyAkaraNa", "stardict-sanskrit-kAvya", "stardict-hindi", "stardict-dictionary-updater", "stardict-sanskrit-student", "stardict-telugu", "stardict-malayalam", "stardict-pali", "stardict-ayurveda", "stardict-marathi", "stardict-tamil", "stardict-gujarati", "stardict-oriya", "stardict-bengali", "stardict-assamese", "stardict-panjabi", "stardict-sinhala", "stardict-prakrit", "stardict-kashmiri", "stardict-kannada", "stardict-divehi", "stardict-nepali", "stardict-urdu", "stardict-indic-update-aur", "StarDict-1", "SanskritDictionariesInstaller", "stardict", "jstardict", "sanDict", "pystardict", "dict-tools", "dict-curation", "indic-dict.github.io", "dsal-scraper", "osx-sanskrit", ],
   "indic-transliteration": ["indic_transliteration_py", "m17n-db-indic",  "sanskrit-fonts", "indic_transliteration_scala",  "sanscript.js"],
   "vvasuki-git": ["buildings", "jananANu", "misc-scala", "sysconf", 
                   "furniture", "misc-c", "rahah", "vlc-addons", 
@@ -106,10 +106,7 @@ def reclone_all_with_submods():
       os.chdir(os.path.dirname(full_path))
       full_path_tmp = os.path.join(GIT_BASE, dir, sub_dir + "_tmp")
       shutil.rmtree(full_path_tmp, ignore_errors=True)
-      command = f"git clone --recurse-submodules -b master {origin_url} {os.path.basename(full_path_tmp)}"
-      logging.info(str(subprocess.check_output(
-        command,
-        stderr=subprocess.STDOUT, shell=True), 'utf-8'))
+      clone_repo(origin_url, os.path.basename(full_path_tmp))
       for f in os.listdir(full_path):
         if str(f).split(".")[-1] in ["ipr", "iml", "iws"]:
           shutil.copy(os.path.join(full_path, f), full_path_tmp)
@@ -118,10 +115,39 @@ def reclone_all_with_submods():
         stderr=subprocess.STDOUT, shell=True), 'utf-8'))
 
 
+def clone_repo(origin_url, dest_path):
+  command = f"git clone --recurse-submodules -b master {origin_url} {dest_path}"
+  logging.info(str(subprocess.check_output(
+    command,
+    stderr=subprocess.STDOUT, shell=True), 'utf-8'))
+
+
+def clone_all_with_submods(groups=None, repos=None):
+  def _clone_all_in_dict(repo_dict):
+    for group, repos in repo_dict.items():
+      if groups is not None and group not in groups:
+        continue
+      for repo in repos:
+        if repos is not None and repo not in repos:
+          continue
+        full_path = os.path.join(GIT_BASE, group, repo)
+        origin_url = f"https://github.com/{group}/{repo}.git"
+        if group == "vvasuki-git":
+          full_path = full_path.replace("gitland/vvasuki/", "gitlandl/vvasuki-git/")
+          origin_url = origin_url.replace("-git", "")
+        if os.path.exists(full_path):
+          logging.info("Skipping " + full_path)
+          continue
+        clone_repo(origin_url=origin_url, dest_path=full_path)
+  _clone_all_in_dict(repo_dict=website_repos)
+  _clone_all_in_dict(repo_dict=reg_repos)
+
+
 if __name__ == '__main__':
   pass
   # set_submodule_branches(sub_dirs=["raw_etexts"])
-  set_submodule_branches()
+  # set_submodule_branches()
+  clone_all_with_submods()
   # fsck_all()
   # pull_all()
   # reclone_all_with_submods()
